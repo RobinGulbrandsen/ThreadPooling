6db38e62b83dc93aeed02f5b632ec90f2fba4806
Blazing Mist <robin@123.no>
2012-10-15 13:19:23 +0200

    Initial Commit
    Uploaded the project

---
+++ Forelesning08/Account.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPooling
+{
+    class Account
+    {
+        private Object thisLock = new Object();
+        int balance;
+
+        Random r = new Random();
+
+        public Account(int initial)
+        {
+            balance = initial;
+        }
+
+        int Withdraw(int amount)
+        {
+
+            // This condition never is true unless the lock statement
+            // is commented out.
+            if (balance < 0)
+            {
+                throw new Exception("Negative Balance");
+            }
+
+            // Comment out the next line to see the effect of leaving out 
+            // the lock keyword.
+            lock (thisLock)
+            {
+                if (balance >= amount)
+                {
+                    Thread.Sleep(200);
+                    Console.WriteLine("Balance before Withdrawal :  " + balance);
+                    Console.WriteLine("Amount to Withdraw        : -" + amount);
+                    balance = balance - amount;
+                    Console.WriteLine("Balance after Withdrawal  :  " + balance);
+                    return amount;
+                }
+                else
+                {
+                    return 0; // transaction rejected
+                }
+            }
+        }
+
+        public void DoTransactions()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Withdraw(r.Next(1, 100));
+            }
+        }
+    }
+}
---
+++ Forelesning08/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPooling
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Starting threads\n");
+            Account acc = new Account(1000);
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                Thread t = new Thread(new ThreadStart(acc.DoTransactions));
+                threads[i] = t;
+            }
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Start();
+            }
+
+            //Hack to wait for the threads to complete
+            while (!isDone(threads))
+            {
+                //Waiting for the method to return true
+
+                /*
+                 * Tell the loop to wait 100milisecounds before It checks again, this will
+                 * make the program run <100ms slower but It will do this to save CPU time to
+                 * other aspects of the program and other prosesses. un-comment the line
+                */
+                //Thread.Sleep(100);
+            }
+
+            Console.WriteLine("\nThreads are done\nPress a key to contineu..");
+            Console.ReadKey();
+        }
+
+        /*
+         *  Sends with the thread array you wish to wait for.  
+         */
+        static bool isDone(Thread[] threads)
+        {
+            int numberOfDone = 0;
+            for (int i = 0; i < threads.Length; i++)
+            {
+                if (!threads[i].IsAlive)
+                {
+                    numberOfDone++;
+                }
+            }
+            if (numberOfDone == threads.Length)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
